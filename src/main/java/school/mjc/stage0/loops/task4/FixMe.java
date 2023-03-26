6cf0d28ccfbc44254f8b12c82c80b78ef4bec6c9
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-26 08:58:44 +0300

    Update FixMe.java

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -1,13 +1,15 @@
 package school.mjc.stage0.loops.task4;
 
 public class FixMe {
+    
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
+        //for (int i = 0; i < 100; i++){
+        for (int i = 0; i <= 7; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             } else if (i == 7) {
                 System.out.println("last");
-                break;
+                //break;
                 System.out.println("will I be printed?");
             }
         }
