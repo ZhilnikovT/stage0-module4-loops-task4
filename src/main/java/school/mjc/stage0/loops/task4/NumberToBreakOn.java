e4dad9786ead63b79237a9469c2b369803423e4e
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-27 12:04:40 +0300

    Update NumberToBreakOn.java

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -1,1 +1,42 @@
-
+public class NumberToBreakOn {
+    
+    public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        
+        boolean isPositiveNumberToGoUntil = numberToGoUntil < 0 ? false : true;
+        boolean isPositiveToBreakWith = toBreakWith < 0 ? false : true;
+        
+        boolean isIteratingTillTheEnd;
+        
+        if (isPositiveNumberToGoUntil){
+            if(isPositiveToBreakWith){
+                isIteratingTillTheEnd = toBreakWith > numberToGoUntil ? true : false;
+            }else{
+                isIteratingTillTheEnd = true;
+            }
+        }else{
+            if(!isPositiveToBreakWith){
+                isIteratingTillTheEnd = toBreakWith < numberToGoUntil ? true : false;
+            }else{
+                isIteratingTillTheEnd = true;
+            }
+        }
+        
+        if(isIteratingTillTheEnd){ System.out.println("iterating till the end");}
+        
+        int i = 0;
+        boolean isCheck = true;
+        do{
+            System.out.println(i);
+            if(isPositiveNumberToGoUntil){ 
+                i++;
+                //System.out.println(isIteratingTillTheEnd);
+                isCheck = (i <= (isIteratingTillTheEnd ? numberToGoUntil: toBreakWith));
+            }else{
+                isCheck = (i > (isIteratingTillTheEnd ? numberToGoUntil: toBreakWith));
+                i--;
+            }
+            //System.out.println(isCheck);
+        }while(isCheck); 
+    }
+    
+}
